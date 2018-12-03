0429a04dc03de586badc2e9ed9b3f998be3d568d
Henning Hall <henning.hall@hotmail.com>
2018-12-03 22:42:11 +0100

    rn web typescript

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+// import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>React Native components</Text>
+      </View>
+    )
+  }
+}
+
+export default App;
