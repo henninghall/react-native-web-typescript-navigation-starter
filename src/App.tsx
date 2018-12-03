5e1a8a20779207069653446a10d14ad0cd8ee69c
Henning Hall <henning.hall@hotmail.com>
2018-12-03 22:46:52 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 // import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
